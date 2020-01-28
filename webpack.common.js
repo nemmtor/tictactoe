a501779f1dcc3b65183c181b175f5bf40754b583
nemmtor <kacper0witas@gmail.com>
2020-01-28 21:29:42 +0100

    Changed assets dir structurem, added path dependency
    Assets:
        - now instead of assets/img.png we have assets/images/img.png and changed assets alias to images alias, styles alias etc.
        - added assets/styles which will hold global styles for example: color variables
        - added styles alias
        - added styles/colors.scss to hold colors variables: main-color, accent-color etc.
    
    Dependencies:
        - added path dependency
    
    Components:
        * Game component:
            - now Game.scss imports colors.scss asset and uses its variables

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,8 @@ module.exports = {
         alias: {
             components: path.resolve(__dirname, 'src/components'),
             utils: path.resolve(__dirname, 'src/utils'),
-            assets: path.resolve(__dirname, 'src/assets'),
+            images: path.resolve(__dirname, 'src/assets/images'),
+            styles: path.resolve(__dirname, 'src/assets/styles'),
         },
         extensions: ['.js', '.jsx'],
     },
