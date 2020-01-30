43df2eae0688443ec195a754656a0ac343bfd6a6
nemmtor <kacper0witas@gmail.com>
2020-01-30 21:35:54 +0100

    Installing why-did-you-render instead of why-did-you-update. Calling whyDidYouRender in index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from 'components';
 
+if (process.env.NODE_ENV !== 'production') {
+    const whyDidYouRender = require('@welldone-software/why-did-you-render');
+    whyDidYouRender(React);
+}
 ReactDOM.render(<App />, document.querySelector('#root'));
